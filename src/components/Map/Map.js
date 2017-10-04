701e9209825bfd63a8b10bc75a6b67fbe9b4dce1
Giora Guttsait <giora111@gmail.com>
2017-10-05 00:50:55 +0300

    Map width is now limited
    * added the bootstrap `container` class to the map's containing div

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -7,7 +7,7 @@ const Map = compose(
   withProps({
     googleMapURL: 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCxOXDhf6CAQsPQC7MHxazygFIxjTAFYsE',
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
+    containerElement: <div style={{ height: `400px` }} className="container"/>,
     mapElement: <div style={{ height: `100%` }} />,
   }),
   withScriptjs,
