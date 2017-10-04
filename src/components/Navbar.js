5717ed34df800fe603befae153517cc275b6fc36
Giora Guttsait <giora111@gmail.com>
2017-10-05 00:54:46 +0300

    Removed NavBar logic to allow dropdown
    was originally designed to allow addition of locations from the map (add -> from map),
    but after adding the map as an integral part of the locations page, this need is removed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,35 +8,27 @@ import {
     MenuItem
 } from 'react-bootstrap';
 
-const MenuDropdown = ({dropdown, itemIndex}) => {
-  const menuItems = dropdown.items.map((item, index) =>
-    <MenuItem key={`dropdown-menu-item-${itemIndex}-${index}`} onClick={item.onClick} disabled={item.disabled || false}>
-      {item.title}
-    </MenuItem>
-  )
-
-  return <NavDropdown eventKey={itemIndex} title={dropdown.title} id={`${dropdown.title}-dropdown`}>
-      {menuItems}
-    </NavDropdown>;
-}
-
 const MenuNavItem = ({nav, navIndex, disabled}) => {
-  return <NavItem eventKey={navIndex} onClick={nav.onClick} disabled={disabled}>{nav.title}</NavItem>
+  return (
+    <NavItem
+      eventKey={navIndex}
+      onClick={nav.onClick}
+      disabled={disabled}
+    >
+      {nav.title}
+    </NavItem>
+  );
 }
 
-const BuildMenuItems = ({items}) => {
-  return items.map((item, index) => {
-    if (item.items && Array.isArray(item.items)) {
-      return <MenuDropdown dropdown={item} itemIndex={index} key={`menu-item-${item.title}-${index}`} />;
-    }
-
-    return <MenuNavItem
+const MenuItems = ({items}) => {
+  return items.map((item, index) => (
+    <MenuNavItem
       nav={item}
       navIndex={index}
       key={`menu-item-${item.title}-${index}`}
       disabled={item.disabled}
-    />;
-  })
+    />
+  ));
 }
 
 const Navbar = ({title = '', items = []}) => (
@@ -49,7 +41,7 @@ const Navbar = ({title = '', items = []}) => (
     </BsNavbar.Header>
     <BsNavbar.Collapse>
       <Nav>
-        <BuildMenuItems items={items} />
+        <MenuItems items={items} />
       </Nav>
     </BsNavbar.Collapse>
   </BsNavbar>
